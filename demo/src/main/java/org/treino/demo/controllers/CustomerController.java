6b63bffe07197e464c35bde8cc8b7011f73588ad
Samuel Oliveira <samuel.oleiv@gmail.com>
2026-09-01 21:35:43 -0300

    Criado os metodos get e update no controller

--- demo/src/main/java/org/treino/demo/controllers/CustomerController.java
+++ demo/src/main/java/org/treino/demo/controllers/CustomerController.java
@@ -5,15 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.treino.demo.dtos.requests.CustomerRequestDTO;
 import org.treino.demo.dtos.responses.CustomerResponseDTO;
-import org.treino.demo.entities.CustomerEntity;
 import org.treino.demo.services.CustomerService;
 
 import jakarta.validation.Valid;
 
 import java.net.URI;
+import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -26,6 +31,16 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @GetMapping
+    public ResponseEntity<Page<CustomerResponseDTO>> getAll(Pageable pageable) {
+        return ResponseEntity.ok(customerService.getAllCustomers(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponseDTO> getById(@PathVariable UUID id) {
+        return ResponseEntity.ok(customerService.getCustomerById(id));
+    }
+
     @PostMapping()
     public ResponseEntity<CustomerResponseDTO> create(@Valid @RequestBody CustomerRequestDTO entity) {
         CustomerResponseDTO createdCustomer = customerService.createCustomer(entity);
@@ -35,5 +50,11 @@ public class CustomerController {
                     .buildAndExpand(createdCustomer.id())
                     .toUri();
         return ResponseEntity.created(uri).body(createdCustomer);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomerResponseDTO> update(@PathVariable UUID id,
+                                                    @Valid @RequestBody CustomerRequestDTO request) {
+        return ResponseEntity.ok(customerService.updateCustomer(id, request));
     }
 }
