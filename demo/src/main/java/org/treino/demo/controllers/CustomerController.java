0ee1dd5643ef8866c2b541f5a3c923a646711d9c
Samuel Oliveira <samuel.oleiv@gmail.com>
2026-09-02 18:19:16 -0300

    Criado o metodo delete no controller do Customer

--- demo/src/main/java/org/treino/demo/controllers/CustomerController.java
+++ demo/src/main/java/org/treino/demo/controllers/CustomerController.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,5 +57,11 @@ public class CustomerController {
     public ResponseEntity<CustomerResponseDTO> update(@PathVariable UUID id,
                                                     @Valid @RequestBody CustomerRequestDTO request) {
         return ResponseEntity.ok(customerService.updateCustomer(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        customerService.deleteCustomer(id);
+        return ResponseEntity.noContent().build();
     }
 }
